bcb2d89b24096e92a0565c41a470ee661895421c
jrm504 <279392356+jrm504@users.noreply.github.com>
2026-07-04 16:02:44 +0700

    docs(all): explain blank broker imports

--- brokers/all/all.go
+++ brokers/all/all.go
@@ -2,9 +2,14 @@
 package all
 
 import (
+	// Register DNSE broker factory via package init.
 	_ "github.com/vnbrokers/vnbrokers-go/brokers/dnse"
+	// Register Entrade broker factory via package init.
 	_ "github.com/vnbrokers/vnbrokers-go/brokers/entrade"
+	// Register FHSC broker factory via package init.
 	_ "github.com/vnbrokers/vnbrokers-go/brokers/fhsc"
+	// Register SSI broker factory via package init.
 	_ "github.com/vnbrokers/vnbrokers-go/brokers/ssi"
+	// Register TCBS broker factory via package init.
 	_ "github.com/vnbrokers/vnbrokers-go/brokers/tcbs"
 )
